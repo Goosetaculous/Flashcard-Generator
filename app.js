e0b4e3fa034850cbb4e9d773716ae3605c5b8e53
Goose <josephtrop@gmail.com>
2017-06-29 18:58:23 -0700

    removed inquirer

--- app.js
+++ app.js
@@ -3,7 +3,6 @@
  */
 (()=>{
 
-    const inquirer = require('inquirer') //Make it fancy to ask a question
     const questions       = require('./question')
     const basicCard =  require('./BasicCard')
     const clozedCard = require('./ClozedCard')
