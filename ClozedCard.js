a0de2ea80ec3d190939a6f4653cac764b036534b
Goose <josephtrop@gmail.com>
2017-06-29 18:54:55 -0700

    here you go

--- ClozedCard.js
+++ ClozedCard.js
@@ -1,3 +1,45 @@
 /**
  * Created by goosetaculous on 6/26/17.
  */
+"use strict";
+(()=>{
+    /**
+     *
+     * @param cloze
+     * @param partial
+     * @param text
+     * @returns {ClozedCard}
+     * @constructor
+     */
+    function ClozedCard(text,cloze){
+        if ( this instanceof ClozedCard ){
+            this.cloze =  cloze
+            this.fullText =  text
+            if(!this.checkValidcloze()){
+                console.log("OOPS this is NOT VALID")
+            }else {
+                this.partial =  this.removedCloze()
+            }
+        }else {
+            return new ClozedCard(text,cloze)
+        }
+    }
+
+    ClozedCard.prototype.cardinfo = function(){
+        console.log(`Cloze of the card is "${this.cloze}"` );
+        console.log(`FullText of the card is "${this.fullText}"` );
+    }
+    ClozedCard.prototype.removedCloze = function(){
+        if (this.checkValidcloze()){
+            return this.fullText.replace(this.cloze,"...")
+        }else{
+            return "This does NOT work"
+        }
+    }
+    ClozedCard.prototype.checkValidcloze=function(){
+        return this.fullText.indexOf(this.cloze) > -1 ?  true :  false
+    }
+
+    module.exports = ClozedCard
+} )();
+
