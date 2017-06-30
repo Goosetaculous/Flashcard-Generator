2ceae8050755b0ea88ad6075e12a3e9213446e9f
Goose <josephtrop@gmail.com>
2017-06-29 18:49:17 -0700

    Final

---
+++ app.js
@@ -0,0 +1,34 @@
+/**
+ * Created by goosetaculous on 6/26/17.
+ */
+(()=>{
+
+    const inquirer = require('inquirer') //Make it fancy to ask a question
+    const questions       = require('./question')
+    const basicCard =  require('./BasicCard')
+    const clozedCard = require('./ClozedCard')
+
+    console.log("DEMO basicCard \n")
+    var firstPresident = new basicCard("Who was the first President of the United States?","George Washington")
+
+    console.log(`Front of the card "${firstPresident.front}"`)
+    console.log(`Back of the card "${firstPresident.back}"`)
+
+    console.log("\nDEMO ClozedCard \n")
+    var valid = new clozedCard("George Washington was the first president of the United States","George Washington")
+
+    //var testCloze2 = new clozedCard("George Washington was the first president of the United States","George Washington")
+    console.log(`Cloze text is "${valid.cloze}"`)
+    console.log(`Partial text is "${valid.partial}"`)
+    console.log(`FullText text is "${valid.fullText}"`)
+    console.log("\nTesting invalid\n")
+    var invalid =  new clozedCard("George Washington was the first president of the United States","James Washington")
+    console.log(`Cloze text is "${invalid.cloze}"`)
+    console.log(`FullText text is "${invalid.fullText}"`)
+
+
+
+
+
+
+})()
